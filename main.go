71b9251a2ba3a179e8e268d3c2bbdbc388c431db
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 19:48:46 +0900

    #1.4 Functions part Two

--- main.go
+++ main.go
@@ -5,15 +5,14 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (lenght int, uppercase string) {
+	defer fmt.Println("I'm done")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	totalLenght, _ := lenAndUpper("nico")
-	fmt.Println(totalLenght)
+	totalLenght, up := lenAndUpper("nico")
+	fmt.Println(totalLenght, up)
 }
