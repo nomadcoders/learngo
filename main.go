82d6a36121c7bdeb13fe2d75308409e20197dd2c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 20:24:43 +0900

    #1.7 Switch

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(18))
 }
