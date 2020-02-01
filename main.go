f002469aa854ac8882a89e65ea44e4aa8ef2065b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 21:24:20 +0900

    #1.9 Arrays and Slices

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 202020
-	fmt.Println(a)
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
