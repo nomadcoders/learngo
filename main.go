b4d045c2cb19196653fd42aed0169cf3612da89c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 20:45:23 +0900

    #1.8 Pointers

--- main.go
+++ main.go
@@ -2,16 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a
+	a = 10
+	fmt.Println(a, *b)
 }
