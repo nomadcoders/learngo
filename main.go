bc96680c0bfba921765e309cb37c6fbaa8ab8a3c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 21:37:43 +0900

    #1.10 Maps

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"nico", "lynn", "dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	nico := map[string]string{"name": "nico", "age": "12"}
+	for key, value := range nico {
+		fmt.Println(key, value)
+	}
 }
