ae449549e7ecf84c1c348be67fe9dc406572ea06
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 19:56:27 +0900

    #1.5 for, range, ...args

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (lenght int, uppercase string) {
-	defer fmt.Println("I'm done")
-	lenght = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLenght, up := lenAndUpper("nico")
-	fmt.Println(totalLenght, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
