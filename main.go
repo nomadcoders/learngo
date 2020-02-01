554cf55a25a56e038662ca17414b65b83154707a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-01 19:39:35 +0900

    #1.3 Functions part One

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	name := "nico"
-	name = "lynn"
-	fmt.Println(name)
+	totalLenght, _ := lenAndUpper("nico")
+	fmt.Println(totalLenght)
 }
