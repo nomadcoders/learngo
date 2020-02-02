79845c005ee8ae716e2dbf8923ab31d78b9597e3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 15:22:45 +0900

    #2.0 Account + NewAccount

---
+++ accounts/accounts.go
@@ -0,0 +1,13 @@
+package accounts
+
+// Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
-
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+import (
+	"github.com/serranoarevalo/learngo/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico.name)
+	account := accounts.NewAccount("nico")
+
 }
