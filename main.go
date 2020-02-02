b88b7b53cab97e4e23670630bc4523063813b123
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 15:03:19 +0900

    #1.11 Structs

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico.name)
 }
