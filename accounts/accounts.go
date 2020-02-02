65d6c9452d53ed7ea45de16fc10067a0524440af
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 15:41:00 +0900

    #2.2 Methods part Two

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,14 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -13,11 +17,20 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -10,4 +10,9 @@ func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
