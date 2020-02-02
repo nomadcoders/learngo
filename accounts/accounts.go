37cd5827c8f2d8fdf9cc0f531ae78ce9c3bd77cd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 15:58:18 +0900

    #2.3 Finishing Up

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,6 +1,9 @@
 package accounts
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Account struct
 type Account struct {
@@ -33,4 +36,18 @@ func (a *Account) Withdraw(amount int) error {
 	}
 	a.balance -= amount
 	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -9,10 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
