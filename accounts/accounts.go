cda63636a5faa6624af366110a4abef073c21e28
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 15:28:25 +0900

    #2.1 Methods part One

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -11,3 +11,13 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Deposit x amount on your account
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/serranoarevalo/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
-
+	account.Deposit(10)
+	fmt.Println(account.Balance())
 }
