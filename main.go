76b75d99f9284eeaa8f25370fed45615f86b2144
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-02-02 17:48:19 +0900

    #3.3 Channels

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	go sexyCount("flynn")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
